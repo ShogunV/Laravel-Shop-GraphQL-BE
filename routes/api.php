a8d22fb2f1152d94298f009cd23e193030ec3285
ShogunV <ivanvasic101@gmail.com>
2021-09-22 17:49:44 +0200

    added login, logout and register endpoints

--- routes/api.php
+++ routes/api.php
@@ -20,27 +20,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [AuthController::class, 'login'])->name('login');
-Route::post('register', [AuthController::class, 'register'])->name('register');
-Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgotPassword');
-Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('resetPassword');
-
-Route::get('products', [ProductController::class, 'index'])->name('products');
-Route::get('products/{id}', [ProductController::class, 'show'])->name('product');
-Route::get('categories', [CategoryController::class, 'index'])->name('categories');
-Route::get('categories/{id}', [CategoryController::class, 'show'])->name('category');
-
-Route::middleware('auth:sanctum')->group(function(){
-    Route::get('user', [AuthController::class, 'user']);
-    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-
-    Route::post('checkout-session', [CheckoutController::class, 'index']);
-
-    // admin routes
-    Route::middleware('admin')->group(function(){
-        Route::apiResource('admin/products', ProductController::class);
-        Route::apiResource('admin/categories', CategoryController::class);
-        Route::get('admin/users', [UserController::class, 'index']);
-        Route::get('admin/orders', [OrderController::class, 'index']);
-    });
-});
